fbc66354a292700d4a973e7380769febcda9f7e4
nithin-hunt <nithinpsgice@gmail.com>
2023-01-22 18:15:48 +0530

    Add DB connection

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+const connectDB = require('./config/db');
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
