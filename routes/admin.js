fb1b5a4b0e7678758f58b4647998db37d653aae3
nithin-hunt <nithinpsgice@gmail.com>
2023-01-22 22:31:01 +0530

    Add delete user route

--- routes/admin.js
+++ routes/admin.js
@@ -9,30 +9,48 @@ router.get("/users", authAdmin, async (req,res) => {
     try {
         const users = await User.find().select("-password -__v");
 	    res.status(200).send({ Users: users });
-    } catch (e) {
+    } catch(e) {
         return res.status(500).json({Error: e.message});
     }
 })
 
 // get user by id
 router.get("/users/:id", [isUserExists, authAdmin], async (req, res) => {
-	const user = await User.findById(req.params.id).select("-password -__v");
-	res.status(200).send({ User: user });
+    try {
+        const user = await User.findById(req.params.id).select("-password -__v");
+        res.status(200).json({ User: user });
+    } catch(e) {
+        return res.status(500).json({Error: e.message});
+    }
 });
 
 // update user by id
 router.put("/users/:id", [isUserExists, authAdmin], async (req, res) => {
-    const {error} = validateUpdateUser(req.body);
-        if (error) {
-            return res.status(400).json({ message: error.details[0].message });
-        }
-  
-	const user = await User.findByIdAndUpdate(
-		req.params.id,
-		{ $set: req.body },
-		{ new: true }
-	).select("-password -__v");
-	res.status(200).send({ data: user, message: "Profile updated successfully" });
+    try {
+        const {error} = validateUpdateUser(req.body);
+            if (error) {
+                return res.status(400).json({ message: error.details[0].message });
+            }
+    
+        const user = await User.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        ).select("-password -__v");
+        res.status(200).send({ data: user, message: "Profile updated successfully" });
+    } catch(e) {
+        return res.status(500).json({Error: e.message});
+    }
+});
+
+// delete user by id
+router.delete("/users/:id", [isUserExists, authAdmin], async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("Successfully deleted user...");
+    } catch(e) {
+        return res.status(500).json({Error: e.message});
+    }
 });
 
 module.exports = router;
