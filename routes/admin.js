abe0901aeda21da55ec24424066211e2f4b8a62e
nithin-hunt <nithinpsgice@gmail.com>
2023-01-23 18:07:26 +0530

    Add pagination for Admin get users

--- routes/admin.js
+++ routes/admin.js
@@ -2,15 +2,63 @@ const router = require('express').Router();
 const User = require('../models/userModel');
 const Meal = require('../models/mealModel');
 const authAdmin = require('../middlewares/authAdmin');
+const paginatedResults = require('../middlewares/paginatedResults');
 const isUserExists = require('../middlewares/isUserExists');
 const {validateUpdateUser} = require('../utils/validators')
 
 // get all users
-router.get("/users", authAdmin, async (req,res) => {
+router.get("/users", [authAdmin], async (req,res) => {
     try {
-        const users = await User.find().select("-password -__v");
-	    res.status(200).send({ Users: users });
+        // const users = await User.find().select("-password -__v");
+        const page = parseInt(req.query.page) - 1 || 0;
+		const limit = parseInt(req.query.limit) || 5;
+		const search = req.query.search || "";
+        let role = req.query.role || "All";
+
+        const roleOptions = [
+            "user",
+            "admin"
+        ];
+
+        role === "All" ? (role = [...roleOptions]) : (role = req.query.role.split(","));
+    
+        const users = await User.find()
+        .where("role")
+        .in([...role])
+        .skip(page * limit)
+        .limit(limit)
+        .select("-password -__v");
+
+        const total = await User.countDocuments({
+            role: { $in: [...role] }
+        });
+
+        const results = {
+            total,
+            page: page + 1,
+            limit,
+            role: role,
+            users,
+        }
+        
+        const startIndex = page * limit
+        const endIndex = (page+1) * limit
+        
+        if(endIndex < total) {
+            results.next = {
+                page: page+2
+            }
+        }
+        
+        if(startIndex > 0) {
+            results.previous = {
+                page: page                
+            }
+        }
+
+	    res.status(200).json(results);
     } catch(e) {
+        console.log(e);
         return res.status(500).json({Error: e.message});
     }
 })
